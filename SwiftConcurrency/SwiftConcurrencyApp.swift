0eecf187ba82424d5ed83617b1e94d345600ac35
dimastepanov <dimastepanov1980@gmail.com>
2023-03-15 06:28:27 +0700

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Dima Stepanov on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
